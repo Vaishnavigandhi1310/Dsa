c83aefa1ae39d8eac41d48dda1a92f5a076d4731
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-09-12 15:44:48 +0530

    done

---
+++ star.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+    int n=5;
+    for(int i=n;i<=i;i--){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+    }
+    System.out.println();
+    }
+}
+}
